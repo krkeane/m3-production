10395a2ef2700cd88d4c1a7d1fe43a29b9e151f2
Kevin R Keane <krkeane@qpm.cc>
2024-04-30 10:30:40 -0400

    Add ignorable error code (zombie order).

--- scripts/error-check.sh
+++ scripts/error-check.sh
@@ -3,7 +3,10 @@
 date=$(date +"/opt/pkg/git/minimal-market-maker/%Y%m%d")
 
 toterr=$(grep error ${date}-*.log | wc | awk '{print $1;}')
-knownerr=$(grep -e " | 404 | " -e " | 104 | " -e " | 201 | " -e " | 202 | " ${date}-*.log | wc | awk '{print $1;}')
+
+# 10147 | OrderId xxx that needs to be cancelled is not found (aka zombie order - GUI knows / backoffice doesnt)
+knownerr=$(grep -e " | 404 | " -e " | 104 | " -e " | 201 | " -e " | 202 | " -e " | 10147 | " ${date}-*.log | wc | awk '{print $1;}')
+
 duperr=$(grep -e " | 103 | " ${date}-*.log | wc | awk '{print $1;}')
 fillerr=$(grep -e " | 10148 | " ${date}-*.log | wc | awk '{print $1;}')
 
