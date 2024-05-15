46ce9798a3878487ccff99e7ca948c390462a6eb
Kevin R Keane <krkeane@qpm.cc>
2024-05-15 16:08:16 -0400

    Parameterize log filename.

--- scripts/ira-sod.sh
+++ scripts/ira-sod.sh
@@ -2,6 +2,8 @@
 set -e
 clear
 
+date=$(date +"%Y%m%d")
+
 cd /opt/pkg/git/minimal-market-maker
 git checkout ira-m3
 mvn clean package install
@@ -10,7 +12,7 @@ printf "\n\n\twaiting for start of day...\n\n"
 while [ $(date +%H:%M) != "09:31" ]; do sleep 1; done
 date
 
-mvn exec:java -Dexec.mainClass="cc.qpm.app.App" -Dexec.args="1001 U19492963 NVDA 55 1" >> 20240513-NVDA.log &
+mvn exec:java -Dexec.mainClass="cc.qpm.app.App" -Dexec.args="1001 U19492963 NVDA 55 1" >> ${date}-NVDA.log &
 sleep 5
 cd -
 
