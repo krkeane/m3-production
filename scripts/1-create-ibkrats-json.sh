e66647fefa63497d6510fd06b487d04b821064b9
Kevin R Keane <krkeane@qpm.cc>
2024-05-17 07:28:02 -0400

    Generate historical report.

--- scripts/1-create-ibkrats-json.sh
+++ scripts/1-create-ibkrats-json.sh
@@ -4,6 +4,7 @@ set -e
 
 # scripts from twsToJson/R
 Rscript -e "rmarkdown::render('load-01.Rmd')"
+Rscript -e "rmarkdown::render('load-02.Rmd')"
 Rscript -e "rmarkdown::render('json-00.Rmd')"
 
 printf "\nIBKR SEC rule 605 files processed.\n"
