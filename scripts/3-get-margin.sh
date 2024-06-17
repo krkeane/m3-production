e7d5b49a3cb8741832874f583bc9841f70492656
Kevin R Keane <krkeane@qpm.cc>
2024-06-17 06:52:10 -0400

    Specify account for margin query.

--- scripts/3-get-margin.sh
+++ scripts/3-get-margin.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+# specify empty account for script to run properly (no existing positions impacting margin)
+export ACCOUNT=U6004887
 export DATA_DIR=${PWD}
 
 
 cd /opt/pkg/git/mm
-mvn exec:java -Dexec.mainClass="cc.qpm.app.MarginUsage" -Dexec.args="${DATA_DIR}/ibkrats.json ${DATA_DIR}/margin.json"
+mvn exec:java -Dexec.mainClass="cc.qpm.app.MarginUsage" -Dexec.args="${ACCOUNT} ${DATA_DIR}/ibkrats.json ${DATA_DIR}/margin.json"
 
 cd ${DATA_DIR}
 
