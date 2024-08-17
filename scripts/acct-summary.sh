9cbad57ed739f1818b77abf748f6634c9ade73db
Kevin R Keane <krkeane@qpm.cc>
2024-08-17 11:31:36 -0400

    Write account summary data to pubsub.

--- scripts/acct-summary.sh
+++ scripts/acct-summary.sh
@@ -5,5 +5,5 @@ set -e
 export DATA_DIR=${PWD}
 
 cd /opt/pkg/git/mm
-mvn exec:java -Dexec.mainClass="cc.qpm.app.AccountSummary"
+mvn exec:java -Dexec.mainClass="cc.qpm.app.AccountSummaryPS"
 cd ${DATA_DIR}
