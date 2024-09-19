32a4f02f5a6c0c2aac435184c3baea568cceb41a
Kevin R Keane <krkeane@qpm.cc>
2024-09-19 09:19:29 -0400

    Trim order size.

--- scripts/ira-sod.sh
+++ scripts/ira-sod.sh
@@ -12,17 +12,19 @@ date
 
 sleep 5 # be sure market is open
 mvn exec:java -Dexec.mainClass="cc.qpm.app.MidPriceHedge" -Dexec.args="9000 U19492963"
-# TODO - quote size should be neutral shares (qty - 99)/2
 
 printf "\n\n\twaiting for start of day...\n\n"
 while [ $(date +%H:%M) != "09:31" ]; do sleep 1; done
 date
 
-mvn exec:java -Dexec.mainClass="cc.qpm.app.App" -Dexec.args="1001 U19492963 SMCI 146 100 6" >> ${date}-SMCI.log &
-#sleep 1
+mvn exec:java -Dexec.mainClass="cc.qpm.app.App" -Dexec.args="1001 U19492963 TSLA 115 100 6" >> ${date}-TSLA.log &
+sleep 1
 
-# mvn exec:java -Dexec.mainClass="cc.qpm.app.App" -Dexec.args="1002 U19492963 TSLA 150 100 6" >> ${date}-TSLA.log &
-sleep 5
+mvn exec:java -Dexec.mainClass="cc.qpm.app.App" -Dexec.args="1002 U19492963 SMCI 100 100 6" >> ${date}-SMCI.log &
+sleep 1
+
+mvn exec:java -Dexec.mainClass="cc.qpm.app.App" -Dexec.args="1003 U19492963 NVDA 160 100 6" >> ${date}-NVDA.log &
+sleep 1
 
 cd -
 
