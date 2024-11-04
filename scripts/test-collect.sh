780a06a232f1b75d866a62933ba2d1e4b721b6d5
Kevin R Keane <krkeane@qpm.cc>
2024-11-04 13:35:53 -0500

    Add scripts.

--- scripts/test-collect.sh
+++ scripts/test-collect.sh
@@ -7,7 +7,7 @@ export DATA_DIR=${PWD}
 cd /opt/pkg/git/twsToJson
 mvn exec:java \
 	-Dexec.mainClass="cc.qpm.app.App" \
-	-Dexec.args="/dev/null ${DATA_DIR}/test.json ${DATA_DIR}/stats-hist-bars.json false ${DATA_DIR}/data-stats-hist-bars.json"
+	-Dexec.args="/dev/null ${DATA_DIR}/test.json ${DATA_DIR}/ba-test.json false ${DATA_DIR}/data-ba-test-hist-bars.json"
 cd ${DATA_DIR}
 
 printf "\nHistorical TRADE data retrieved from TWS API.\n"
