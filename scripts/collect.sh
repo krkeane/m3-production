e9113b3e0d4020978fc860f47e0e6855c84c5c50
Kevin R Keane <krkeane@qpm.cc>
2024-11-07 10:05:52 -0500

    Update statistical analysis.

--- scripts/collect.sh
+++ scripts/collect.sh
@@ -7,7 +7,7 @@ export DATA_DIR=${PWD}
 cd /opt/pkg/git/twsToJson
 mvn exec:java \
 	-Dexec.mainClass="cc.qpm.app.App" \
-	-Dexec.args="/dev/null ${DATA_DIR}/test.json ${DATA_DIR}/collect.json false ${DATA_DIR}/data-hist-bars.json"
+	-Dexec.args="/dev/null ${DATA_DIR}/20241106.json ${DATA_DIR}/collect.json false ${DATA_DIR}/data-hist-bars.json"
 cd ${DATA_DIR}
 
 printf "\nHistorical TRADE data retrieved from TWS API.\n"
