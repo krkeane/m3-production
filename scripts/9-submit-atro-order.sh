79123dafcd09a6012dfb8320283f79d3da52adab
Kevin R Keane <krkeane@qpm.cc>
2025-09-20 12:20:45 -0400

    Updated code.

--- scripts/9-submit-atro-order.sh
+++ scripts/9-submit-atro-order.sh
@@ -2,13 +2,22 @@
 
 set -e
 
+date=$(date +"%Y%m%d")
+clear
+
+printf "\n\n\tATRO daily order entry\n\n\twaiting for 09:30 ...\n\n"
+while [ $(date +%H:%M) != "09:30" ]; do sleep 1; done
+date
+
+
 export DATA_DIR=${PWD}
 
 cd /opt/pkg/git/atro-daily
+
 mvn exec:java \
-	-Dexec.mainClass="cc.qpm.app.SubmitIbkrAts" \
-	-Dexec.args=""
+	-Dexec.mainClass="cc.qpm.app.SubmitNASDAQ" \
+	-Dexec.args="${DATA_DIR}/atro.json"
 
 cd ${DATA_DIR}
-printf "\nATRO IBKR ATS order staged.\n"
+printf "\nATRO order submitted.\n"
 
